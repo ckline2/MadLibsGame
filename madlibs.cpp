faa9f9b9c2a0f15116c826805d49a078db1d46db
ckline2 <ckline2@mail.csuchico.edu>
2018-09-14 09:59:47 -0700

    madlibs exercise6

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,30 @@
-//Authors: 
-#include <iostream>
 #include <string>
+#include <iostream>
+using namespace std;
 
 int main()
 {
-
-  return 0;
+    int number;
+    cout<<"enter a number"<<endl;
+    cin>>number;
+    string name;
+    cout<<"enter a town name"<<endl;
+    cin>>name;
+    string name2;
+    cout<<"enter a man name"<<endl;
+    cin>>name2;
+    string adjective;
+    cout<<"enter a adjective"<<endl;
+    cin>>adjective;
+    string noun;
+    cout<<"enter a noun"<<endl;
+    cin>>noun;
+    cout<<number<<" years ago in town called ";
+    cout<<name<<" there was a man named ";
+    cout<<name2<<endl;
+    cout<<name2<<" was a very ";
+    cout<<adjective<<" man who lived ";
+    cout<<noun<<endl;
+    return 0;        
 }
+
